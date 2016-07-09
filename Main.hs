24f0e40e2fd31fda00365cce6fbe77b7561dc5b7
Michel Rivas Hernandez <M.Rivas1@uni.brighton.ac.uk>
2016-07-09 14:58:50 +0100

    Improving server

--- Main.hs
+++ Main.hs
@@ -23,20 +23,25 @@ module Main (
 
 -- | Simple function to create a hello message.
 -- prop> stripPrefix "Hello " (hello s) == Just s
-import Network (listenOn, withSocketsDo, accept, connectTo, sClose, PortID(..), Socket)
+import Network (listenOn, withSocketsDo, accept, connectTo, sClose, PortID(..), Socket,PortNumber)
 import System.Environment (getArgs)
 import System.IO (hSetBuffering, hGetLine, hPutStrLn, hClose, BufferMode(..), Handle)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.MVar
 import Control.Monad
 
-data State1 = State1 {
-    proposalNumber :: Int,
-    clientHandles :: [Handle]
+type ServerID = String
+
+data Server = Server {
+    serverID :: ServerID,
+    serverHandle :: Handle,
+    host :: String,
+    port :: PortNumber
 }
 
 data State = State {
-    message :: Int
+    proposalNumber :: Int,
+    serverList :: [Server]
 }
 
 main :: IO ()
@@ -45,15 +50,38 @@ main = withSocketsDo $ do
     let port = fromIntegral (read $ head args :: Int)
     socket <- listenOn $ PortNumber port
     putStrLn $ "Listening on " ++ (head args)
-    let state = State {message = 1}
+    let state = State {proposalNumber = 1, serverList = []}
     cfg <- newMVar state
-    forever $ accept socket >>= forkIO . (handleClientRequest socket cfg)
+    forever $ accept socket >>= forkIO . (handleClientConnection socket cfg)
 
-handleClientRequest socket cfg (handle, host, portno) = do
+handleClientConnection socket cfg (handle, host, portno) = do
+    putStrLn "Client connected"
+    state <- takeMVar cfg
+    let servers = serverList state
+    id <- hGetLine handle
+    putStrLn $ "ID: " ++ id
+    let server = Server {serverID = id, serverHandle = handle, host = host, port = portno}
+    let newState = state { serverList = server : servers}
+    putMVar cfg newState
+    handleClientRequest socket cfg server
+
+handleClientRequest socket cfg server = do
+    let handle = serverHandle server
     msg <- hGetLine handle
     state <- takeMVar cfg
-    let id = message state
-    putStrLn $ show id ++ host ++ " says: " ++ msg
-    let newState = state { message = id + 1}
+    let proposal = proposalNumber state
+    let line = (serverID server) ++ " says: " ++ msg
+    let servers = serverList state
+    broadcast line (serverID server) servers
+    let newState = state { proposalNumber = proposal + 1}
     putMVar cfg newState
-    handleClientRequest socket cfg (handle, host, portno)
+    handleClientRequest socket cfg server
+
+send msg s = do
+    hPutStrLn (serverHandle s) msg
+    putStrLn msg
+
+broadcast _ _ [] = return ()
+broadcast msg origin (s : servers) = do
+    when (serverID s /= origin) $ send msg s
+    broadcast msg origin servers
